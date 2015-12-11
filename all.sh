95537a300c0c2bb7852d0734219ff1c42edeafdc
Huamin Chen <hchen@redhat.com>
2015-12-11 15:07:51 -0500

    add run script

---
+++ all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=/srv/ceph
+VAR=/srv/ceph-var
+IP=10.1.4.12
+NET=10.1.4.0/24
+
+rm -rf ${VAR}/* /etc/ceph/*
+
+docker run -d --net=host -e MON_IP=${IP}  -e CEPH_PUBLIC_NETWORK=${NET} -e CEPH_DAEMON=mon  -v /etc/ceph:/etc/ceph -v ${VAR}:/var/lib/ceph rhcs
+
+for i in 0 1 2
+do
+   umount /tmp/ceph_disk${i}
+   dd if=/dev/zero of=${DIR}/d${i} bs=256M count=5 conv=notrunc
+   mkfs -t xfs -f ${DIR}/d${i}
+   mkdir -p /tmp/ceph_disk${i}
+   mount -t xfs -o loop ${DIR}/d${i} /tmp/ceph_disk${i}
+   docker run -d --privileged --pid=host --net=host -e MON_IP=${IP}  -e CEPH_DAEMON=osd -e  OSD_TYPE=directory -v /tmp/ceph_disk${i}:/var/lib/ceph/osd/ -v ${VAR}:/var/lib/ceph -v /etc/ceph:/etc/ceph rhcs
+done
+
+
+#ceph -w 
+#docker stop $(docker ps -q)
